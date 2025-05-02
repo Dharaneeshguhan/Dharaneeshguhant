9275d73198dd83148ef406c853b513eb93348baf
Dharaneesh guhan <dharaneeshguhan.t2023ece@sece.ac.in>
2025-05-02 07:00:25 +0530

    Resolve merge conflict in postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
-export default {
-=======
 module.exports = {
->>>>>>> 1ea0da839a7b39b08ea98062b150ade3283d158b
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
+
